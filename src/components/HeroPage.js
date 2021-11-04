6bca88b2616181131a20b0055e51f6dbfaf6598d
eze-une <mruniverse1x@gmail.com>
2021-11-04 11:10:00 +0300

    changed the background and look of heroPage

--- src/components/HeroPage.js
+++ src/components/HeroPage.js
@@ -7,7 +7,7 @@ function HeroPage() {
     return (
         <div className="hero-page">
             <div className='hero-page-container'>
-                <h1 className="title">We bring  you all the travel options to a signle platform!</h1>
+                <h1 className="title">The Adventure awaits!</h1>
                 <h3 className="description">Find your ideal trip from many of the travel pacages.</h3>
                 <div className='hero-page-button'>
                     <Link to="/signup" className="hero-page-signup"> Sign Up</Link>
