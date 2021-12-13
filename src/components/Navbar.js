22d7ffed69f345eec9ae05f5be60b00421303613
Hiwot Medhin <hiwotmedhings@gmail.com>
2021-12-13 23:49:08 +0300

    Clean up navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,92 +2,69 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "./Button";
 import "./Navbar.css";
-import {sendAmplitudeData} from './utilities/amplitude';
+import { sendAmplitudeData } from "./utilities/amplitude";
 
 function Navbar() {
-  const [click, setClick] = useState(false);
-  const [button, setButton] = useState(true);
-  const [navbar, setNavbar] = useState(false);
+	const [click, setClick] = useState(false);
+	const [button, setButton] = useState(true);
+	const [navbar, setNavbar] = useState(false);
 
-  const handleClick = () => setClick(!click);
-  const closeMobileMenu = () => setClick(false);    
-  const exploreButton = () => {
-      var event = "Explore Button Clicked";
-      var eventProperties = {
-          "Test": "Hello"
-      };
-      console.log(event, eventProperties);
-      setClick(false);
-      sendAmplitudeData(event, eventProperties);
-  };
+	const handleClick = () => setClick(!click);
+	const closeMobileMenu = () => setClick(false);
+	const exploreButton = () => {
+		var event = "Explore Button Clicked";
+		var eventProperties = {
+			Test: "Hello",
+		};
+		console.log(event, eventProperties);
+		setClick(false);
+		sendAmplitudeData(event, eventProperties);
+	};
 
-  const showButton = () => {
-    if (window.innerWidth <= 700) {
-      setButton(false);
-    } else {
-      setButton(true);
-    }
-  };
-  window.addEventListener("resize", showButton);
+	const showButton = () => {
+		if (window.innerWidth <= 700) {
+			setButton(false);
+		} else {
+			setButton(true);
+		}
+	};
+	window.addEventListener("resize", showButton);
 
-  const changeBackground = () => {
-    console.log(window.scrollY);
-  };
-  window.addEventListener("srcoll", changeBackground);
+	const changeBackground = () => {
+		console.log(window.scrollY);
+	};
+	window.addEventListener("srcoll", changeBackground);
 
-    return (
-        <>
-            <nav className="navbar">
-                <div className="navbar-container">
-                    <Link to="/" className="navbar-logo">
-                        Utentic
-                    </Link>
-                    <div className="menu-icon" onClick={handleClick}>
-                        <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
-                    </div>
-                    <ul className={click ? "nav-menu active" : "nav-menu"}>
-                        <li className="name-item">
-                            <Link
-                                to="/"
-                                className="nav-links"
-                                onClick={closeMobileMenu}
-                            >
-                                Home
-                            </Link>
-                        </li>
-                        <li className="name-item">
-                            <Link
-                                to="/explore"
-                                className="nav-links"
-                                onClick={exploreButton}
-                            >
-                                Explore
-                            </Link>
-                        </li>
-                        <li className="name-item">
-                            <Link
-                                to="/services"
-                                className="nav-links"
-                                onClick={closeMobileMenu}
-                            >
-                                Services
-                            </Link>
-                        </li>
-                        <li className="name-item">
-                            <Link
-                                to="/signup"
-                                className="nav-links-mobile"
-                                onClick={closeMobileMenu}
-                            >
-                                Sign Up
-                            </Link>
-                        </li>
-                    </ul>
-                    {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
-                </div>
-            </nav>
-        </>
-    );
+	return (
+		<>
+			<nav className="navbar">
+				<div className="navbar-container">
+					<Link to="/" className="navbar-logo">
+						UTENTIC
+					</Link>
+					<div className="menu-icon" onClick={handleClick}>
+						<i className={click ? "fas fa-times" : "fas fa-bars"}></i>
+					</div>
+					<ul className={click ? "nav-menu active" : "nav-menu"}>
+						<li className="name-item">
+							<Link to="/" className="nav-links" onClick={closeMobileMenu}>
+								Home
+							</Link>
+						</li>
+						<li className="name-item">
+							<Link to="/explore" className="nav-links" onClick={exploreButton}>
+								Explore
+							</Link>
+						</li>
+					</ul>
+
+					<button class="ui inverted button">
+						<a href="http://bit.ly/31YBqZU">Join Us On Telegram</a>
+					</button>
+				</div>
+			</nav>
+		</>
+	);
 }
 
 export default Navbar;
