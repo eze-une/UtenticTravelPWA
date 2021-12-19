928ea1e14d4c3b58e43c268a34085798327d19e5
eze-une <mruniverse1x@gmail.com>
2021-12-19 22:05:24 +0300

    added price data

--- src/components/CardInfoData.js
+++ src/components/CardInfoData.js
@@ -4,89 +4,104 @@ export const CardInfoData=[
         text:"Explore the hidden Waterfalls of Ensaro",
         label: "Adventure",
         path:"/services" ,  
+        price: '400'
     },
     {
         src:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWWUboXNc5BHzUXNfglewUktNqr5Cn9E8wzw&usqp=CAU',
         text:"Explore the hidden Waterfalls of Ensaro",
         label: "Adventure",
         path:"/services" ,  
+        price: '800'
+    },
+    {
+        src:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWWUboXNc5BHzUXNfglewUktNqr5Cn9E8wzw&usqp=CAU',
+        text:"Explore the hidden Waterfalls of Ensaro",
+        label: "Adventure",
+        path:"/services" ,
+        price: '300'  
+    },
+    {
+        src:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWWUboXNc5BHzUXNfglewUktNqr5Cn9E8wzw&usqp=CAU',
+        text:"Explore the hidden Waterfalls of Ensaro",
+        label: "Adventure",
+        path:"/services" , 
+        price: '300' 
     },
     {
         src:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWWUboXNc5BHzUXNfglewUktNqr5Cn9E8wzw&usqp=CAU',
         text:"Explore the hidden Waterfalls of Ensaro",
         label: "Adventure",
         path:"/services" ,  
+        price: '300'
     },
     {
         src:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWWUboXNc5BHzUXNfglewUktNqr5Cn9E8wzw&usqp=CAU',
         text:"Explore the hidden Waterfalls of Ensaro",
         label: "Adventure",
         path:"/services" ,  
+        price: '300'
+    },
+    {
+        src:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWWUboXNc5BHzUXNfglewUktNqr5Cn9E8wzw&usqp=CAU',
+        text:"Explore the hidden Waterfalls of Ensaro",
+        label: "Adventure",
+        path:"/services" , 
+        price: '300' 
+    },
+    {
+        src:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWWUboXNc5BHzUXNfglewUktNqr5Cn9E8wzw&usqp=CAU',
+        text:"Explore the hidden Waterfalls of Ensaro",
+        label: "Adventure",
+        path:"/services" , 
+        price: '300' 
     },
     {
         src:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWWUboXNc5BHzUXNfglewUktNqr5Cn9E8wzw&usqp=CAU',
         text:"Explore the hidden Waterfalls of Ensaro",
         label: "Adventure",
         path:"/services" ,  
+        price: '500'
+    },
+    {
+        src:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWWUboXNc5BHzUXNfglewUktNqr5Cn9E8wzw&usqp=CAU',
+        text:"Explore the hidden Waterfalls of Ensaro",
+        label: "Adventure",
+        path:"/services" , 
+        price: '500' 
+    },
+    {
+        src:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWWUboXNc5BHzUXNfglewUktNqr5Cn9E8wzw&usqp=CAU',
+        text:"Explore the hidden Waterfalls of Ensaro",
+        label: "Adventure",
+        path:"/services" ,
+        price: '500'  
+    },
+    {
+        src:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWWUboXNc5BHzUXNfglewUktNqr5Cn9E8wzw&usqp=CAU',
+        text:"Explore the hidden Waterfalls of Ensaro",
+        label: "Adventure",
+        path:"/services" , 
+        price: '600' 
+    },
+    {
+        src:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWWUboXNc5BHzUXNfglewUktNqr5Cn9E8wzw&usqp=CAU',
+        text:"Explore the hidden Waterfalls of Ensaro",
+        label: "Adventure",
+        path:"/services" , 
+        price: '950' 
+    },
+    {
+        src:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWWUboXNc5BHzUXNfglewUktNqr5Cn9E8wzw&usqp=CAU',
+        text:"Explore the hidden Waterfalls of Ensaro",
+        label: "Adventure",
+        path:"/services" , 
+        price: '1200' 
     },
     {
         src:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWWUboXNc5BHzUXNfglewUktNqr5Cn9E8wzw&usqp=CAU',
         text:"Explore the hidden Waterfalls of Ensaro",
         label: "Adventure",
         path:"/services" ,  
-    },
-    {
-        src:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWWUboXNc5BHzUXNfglewUktNqr5Cn9E8wzw&usqp=CAU',
-        text:"Explore the hidden Waterfalls of Ensaro",
-        label: "Adventure",
-        path:"/services" ,  
-    },
-    {
-        src:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWWUboXNc5BHzUXNfglewUktNqr5Cn9E8wzw&usqp=CAU',
-        text:"Explore the hidden Waterfalls of Ensaro",
-        label: "Adventure",
-        path:"/services" ,  
-    },
-    {
-        src:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWWUboXNc5BHzUXNfglewUktNqr5Cn9E8wzw&usqp=CAU',
-        text:"Explore the hidden Waterfalls of Ensaro",
-        label: "Adventure",
-        path:"/services" ,  
-    },
-    {
-        src:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWWUboXNc5BHzUXNfglewUktNqr5Cn9E8wzw&usqp=CAU',
-        text:"Explore the hidden Waterfalls of Ensaro",
-        label: "Adventure",
-        path:"/services" ,  
-    },
-    {
-        src:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWWUboXNc5BHzUXNfglewUktNqr5Cn9E8wzw&usqp=CAU',
-        text:"Explore the hidden Waterfalls of Ensaro",
-        label: "Adventure",
-        path:"/services" ,  
-    },
-    {
-        src:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWWUboXNc5BHzUXNfglewUktNqr5Cn9E8wzw&usqp=CAU',
-        text:"Explore the hidden Waterfalls of Ensaro",
-        label: "Adventure",
-        path:"/services" ,  
-    },
-    {
-        src:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWWUboXNc5BHzUXNfglewUktNqr5Cn9E8wzw&usqp=CAU',
-        text:"Explore the hidden Waterfalls of Ensaro",
-        label: "Adventure",
-        path:"/services" ,  
-    },
-    {
-        src:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWWUboXNc5BHzUXNfglewUktNqr5Cn9E8wzw&usqp=CAU',
-        text:"Explore the hidden Waterfalls of Ensaro",
-        label: "Adventure",
-        path:"/services" ,  
-    },
-    {
-        src:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWWUboXNc5BHzUXNfglewUktNqr5Cn9E8wzw&usqp=CAU',
-        text:"Explore the hidden Waterfalls of Ensaro",
-        label: "Adventure",
-        path:"/services" ,  
+        price: '900'
     }
 ]
