a109346420840a67cb0b8314fd9c665c4dab0e36
Hiwot Medhin <hiwotmedhings@gmail.com>
2021-12-19 21:38:26 +0300

    Finish footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,42 +1,52 @@
 import React from "react";
 import "./Footer.css";
+import telegram from "../telegram.png";
+import instagram from "../instagram.png";
+import facebook from "../facebook.png";
+import { Link } from "react-router-dom";
 
 function Footer() {
-  return (
-    <div className="footer-container">
-      {/* <div className="info-conatiner">
-        <div className="footer-info">
-          <div className="footer-element">
-            <h1 className="footer-head">Utentic</h1>
-            <p>
-              {`Check out the best hiking offers and Enjoy the Best Adventure.
-            Choose by price and destination and find the trip that works for you`}
-            </p>
-          </div>
+	return (
+		<div className="footer-container">
+			<div className="footer-items">
+				<div className="footer-logo">
+					<h1>UTENTIC</h1>
+					<h4 style={{fontWeight: "lighter"}}>Experience the Authentic!</h4>
+					<br />
+					<p>Contact Us</p>
+					<hr /> <br />
+					<div>
+						<p>
+							Email us: <a href="mailto:travel.utentic@gmail.com">
+								travel.utentic@gmail.com
+							</a>
+						</p>
+						<p>
+							Send us a message on telegram: <a href="https://t.me/utentic">@utentic</a>
+						</p>
+					</div>
+				</div>
 
-          <div className="footer-element">
-            <h2 className="footer-head">Company</h2>
-            <div>
-              <h3>mobile</h3>
-              <h3>ssthn</h3>
-            </div>
-          </div>
-
-          <div className="footer-element">
-            <h2 className="footer-head">Contact</h2>
-            <h3>utentictravel@gmail.com</h3>
-            <h3>Affiliates</h3>
-          </div>
-
-          <div className="footer-element">
-            <h2 className="footer-head">More</h2>
-            <h3>Packages</h3>
-            <h3>Discounted Packages</h3>
-          </div>
-        </div>
-      </div> */}
-    </div>
-  );
+				<div className="footer-socials">
+					<div className="footer-socials-container">
+						<a href="https://t.me/travelethiopiautentic">
+							<img src={telegram} className="socials-icon" alt="telegram" />
+						</a>
+						<a href="http://instagram.com/travelethiopiautentic">
+							<img src={instagram} className="socials-icon" alt="instagram" />
+						</a>
+						<a href="http://facebook.com/travelethiopiautentic">
+							<img
+								src={facebook}
+								className="socials-icon-facebook"
+								alt="facebook"
+							/>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Footer;
